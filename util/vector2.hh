e809bec64133f33cffd7fd47faa6a5acc9f5542c
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-15 11:07:50 -0600

    Add util classes

---
+++ util/vector2.hh
@@ -0,0 +1,17 @@
+//  Copyright 2016 Martin Fracker, Jr.
+//  All Rights Reserved.
+// 
+//  This project is free software, released under the terms
+//  of the GNU General Public License v3. Please see the
+//  file LICENSE in the root directory or visit
+//  www.gnu.org/licenses/gpl-3.0.en.html for license terms.
+
+#pragma once
+
+struct Vector2 {
+  int x, y;
+};
+
+inline Vector2 operator-(Vector2 vector2) {
+  return { -vector2.x, -vector2.y };
+}
